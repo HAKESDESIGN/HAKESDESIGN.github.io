73028ace02f80828d55a2c1d8769d1c8e811ad27
HAKESDESIGN <james@hakes.design>
2021-04-01 15:49:17 +1300

    Website Config Test
    Website Config Test

---
+++ HH44_CONFIG_XR_test20210401/keyshot-configurator-imagemapping.js
@@ -0,0 +1,696 @@
+imagemappingdata =
+[
+    {
+        "image": "HH44_CONFIG_XR_test20210401.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+11"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+11"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+11"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+12"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+12"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+12"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+13"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+13"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.9.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+13"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.10.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+14"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.11.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+14"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.12.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+14"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.13.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+15"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.14.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+15"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.15.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+15"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.16.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+16"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.17.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+16"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.18.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+16"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.19.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+17"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.20.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+17"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.21.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+17"
+        ],
+        "studio": 1715717509
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.22.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+11"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.23.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+11"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.24.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+11"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.25.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+12"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.26.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+12"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.27.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+12"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.28.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+13"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.29.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+13"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.30.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+13"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.31.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+14"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.32.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+14"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.33.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+14"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.34.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+15"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.35.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+15"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.36.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+15"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.37.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+16"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.38.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+16"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.39.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+16"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.40.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+17"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.41.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+17"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.42.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+17"
+        ],
+        "studio": 1715717510
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.43.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+11"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.44.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+11"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.45.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+11"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.46.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+12"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.47.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+12"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.48.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+12"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.49.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+13"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.50.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+13"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.51.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+13"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.52.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+14"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.53.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+14"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.54.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+14"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.55.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+15"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.56.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+15"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.57.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+15"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.58.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+16"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.59.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+16"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.60.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+16"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.61.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+102",
+            "18+17"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.62.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+103",
+            "18+17"
+        ],
+        "studio": 1715717500
+    },
+    {
+        "image": "HH44_CONFIG_XR_test20210401.63.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "105+104",
+            "18+17"
+        ],
+        "studio": 1715717500
+    }
+];
---
+++ HH44_CONFIG_XR_test20210401/keyshot-configurator-modeldata.js
@@ -0,0 +1,117 @@
+model =
+{
+    "materialgroups": [
+        {
+            "materials": [
+                {
+                    "icon": "snow%20storm_745630.png",
+                    "multimaterials": [
+                        "18+11"
+                    ],
+                    "name": "Snow Storm_745630"
+                },
+                {
+                    "icon": "silver%20leaf_745529.png",
+                    "multimaterials": [
+                        "18+12"
+                    ],
+                    "name": "Silver Leaf_745529"
+                },
+                {
+                    "checked": true,
+                    "icon": "paint%20metallic%20grey%20%231.png",
+                    "multimaterials": [
+                        "18+13"
+                    ],
+                    "name": "Paint Metallic Grey #1"
+                },
+                {
+                    "icon": "paint%20metallic%20amber.png",
+                    "multimaterials": [
+                        "18+14"
+                    ],
+                    "name": "Paint Metallic Amber"
+                },
+                {
+                    "icon": "paint%20metallic%20candy%20blue.png",
+                    "multimaterials": [
+                        "18+15"
+                    ],
+                    "name": "Paint Metallic Candy Blue"
+                },
+                {
+                    "icon": "paint%20metallic%20lime%20green.png",
+                    "multimaterials": [
+                        "18+16"
+                    ],
+                    "name": "Paint Metallic Lime Green"
+                },
+                {
+                    "icon": "paint%20metallic%20red.png",
+                    "multimaterials": [
+                        "18+17"
+                    ],
+                    "name": "Paint Metallic Red"
+                }
+            ],
+            "name": "Hull Colour"
+        },
+        {
+            "materials": [
+                {
+                    "icon": "paint%20gloss%20black%20%235.png",
+                    "multimaterials": [
+                        "105+102"
+                    ],
+                    "name": "Paint Gloss Black #5"
+                },
+                {
+                    "checked": true,
+                    "icon": "paint%20gloss%20red.png",
+                    "multimaterials": [
+                        "105+103"
+                    ],
+                    "name": "Paint Gloss Red"
+                },
+                {
+                    "icon": "paint%20gloss%20white%20%232.png",
+                    "multimaterials": [
+                        "105+104"
+                    ],
+                    "name": "Paint Gloss White #2"
+                }
+            ],
+            "name": "Boot Top"
+        }
+    ],
+    "products": [
+        {
+            "checked": true,
+            "icon": "hh44%20parent%20model.png",
+            "materialgroups": [
+                "Hull Colour",
+                "Boot Top"
+            ],
+            "modelset": 1005,
+            "name": "HH44 Parent Model"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "sunny%20aft%20port.png",
+            "id": 1715717500,
+            "name": "SUNNY AFT PORT"
+        },
+        {
+            "icon": "sunny%20fwd%20port.png",
+            "id": 1715717509,
+            "name": "SUNNY FWD PORT"
+        },
+        {
+            "checked": true,
+            "icon": "sunny%20starboard.png",
+            "id": 1715717510,
+            "name": "SUNNY STARBOARD"
+        }
+    ]
+};
